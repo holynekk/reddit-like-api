abda43eb9c7729ce5e38bf7ee2a9d0b8fa00c9dd
holynekk <e238109@metu.edu.tr>
2022-03-17 16:10:55 +0300

    posts endpoint CRUD operations

--- api/main.py
+++ api/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from .models import Base
+from . import models
+from .database import engine
+from .router import post
+
+models.Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
+
+app.include_router(post.router)
 
 @app.get("/")
 def root():
--- api/router/post.py
+++ api/router/post.py
@@ -1,8 +1,55 @@
-from fastapi import APIRouter
+from pyexpat import model
+from fastapi import APIRouter, Depends, status, HTTPException, Response
+from sqlalchemy.orm import Session
+from typing import List
+from ..database import get_db
+from .. import models, schemas
 
 router = APIRouter(prefix="/posts", tags=['Posts'])
 
-@router.get('/')
-def get_posts():
-    return {"message": "success"}
+# Get posts
+@router.get('/', response_model=List[schemas.PostOut])
+def get_posts(db: Session = Depends(get_db)):
+    all_posts = db.query(models.Post).all()
+    return all_posts
 
+@router.get('/{id}', response_model=schemas.PostOut)
+def get_posts(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} was not found!")
+    return post
+
+# Create posts
+@router.post('/', status_code=status.HTTP_201_CREATED)
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
+    created_post = models.Post(**post.dict())
+    db.add(created_post)
+    db.commit()
+    db.refresh(created_post)
+    return created_post
+
+# Update post
+@router.put("/{id}", response_model=schemas.PostOut)
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id==id)
+    
+    if not post.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} was not found!")
+    post.update(updated_post.dict(), synchronize_session=False)
+    # print("-------------------------")
+    # print(post.first())
+    # print("-------------------------")
+    db.commit()
+    return post.first()
+
+# Delete Post
+@router.post('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, db: Session = Depends(get_db)):
+    print(id)
+    post = db.query(models.Post).filter(models.Post.id==id)
+    if not post.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} was not found!")
+    post.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
